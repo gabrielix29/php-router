45784cc1974f4a97a4689bdd221c2a73adbeaf67
gabrielix29 <gabriel@guillet.ch>
2020-08-05 13:20:17 +0200

    add add and run method

--- src/Router.php
+++ src/Router.php
@@ -2,6 +2,49 @@
 
 namespace gabrielix29\Router;
 
-class Router {
+class Router
+{
+    private array $routes;
+    private array $allowedHttpMethods = ["GET", "POST", "PUT", "DELETE", "ALL"];
 
+    function __construct()
+    {
+        $this->routes = array();
+    }
+
+    public function add(string $method, string $route, callable $closure)
+    {
+        //check if method is valid
+        if (in_array($method, $this->allowedHttpMethods)) {
+            array_push($this->routes, [
+                "method" => $method,
+                "route" => $route,
+                "closure" => $closure
+            ]);
+        }
+    }
+
+    public function run()
+    {
+        $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+        $requestMethod = $_SERVER['REQUEST_METHOD'];
+        $uriParts = explode('/', $uri);
+        $routeFound = null;
+
+        foreach ($this->routes as $route) {
+            if (preg_match($route['route'], $uri)) {
+                if ($route['method'] == $requestMethod || $route['method'] == "ALL") {
+                    $routeFound = $route;
+                    break;
+                }
+            }
+        }
+
+        if (!$routeFound) {
+            http_response_code(404);
+            exit();
+        }
+
+        $routeFound['closure']($uriParts);
+    }
 }
